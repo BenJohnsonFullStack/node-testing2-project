5edce78209bc2e0e563977227559ec6deabcbf08
Ben Johnson <monk.sound.design@gmail.com>
2023-09-19 17:18:19 -0400

    database wired

--- api/pets/pets-router.js
+++ api/pets/pets-router.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const Pet = require("./pets-model");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.json("Hello from pets router");
+router.get("/", async (req, res, next) => {
+  const pets = await Pet.get();
+  res.json(pets);
+});
+
+router.get("/:id", async (req, res, next) => {
+  res.json("Hello from pets router GET /:id");
+});
+
+router.post("/", async (req, res, next) => {
+  res.status(201).json("Hello from pets router POST /");
+});
+
+router.put("/:id", async (req, res, next) => {
+  res.status(204).json("Hello from pets router PUT /:id");
+});
+
+router.delete("/:id", async (req, res, next) => {
+  res.status(204).json("Hello from pets router DELETE /:id");
 });
 
 module.exports = router;
