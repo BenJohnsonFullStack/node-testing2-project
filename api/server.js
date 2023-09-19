56e7580b70d10e925245008245913467a61c2835
Ben Johnson <monk.sound.design@gmail.com>
2023-09-19 17:05:25 -0400

    server wired

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const petsRouter = require("./pets/pets-router");
+
+const server = express();
+
+server.use(express.json());
+// server.use("/api/pets", petsRouter);
+
+server.use("*", (req, res) => {
+  res.json("Hello from pets");
+});
+
+// ERROR HANDLING MIDDLEWARE //
+// eslint-disable-next-line no-unused-vars
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: "Something went wrong",
+  });
+});
+// ERROR HANDLING MIDDLEWARE //
+
+module.exports = server;
